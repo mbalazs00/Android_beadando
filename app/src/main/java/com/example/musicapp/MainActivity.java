81af2f7a3f95aca63c2b4197bc0c0191d7f15bdf
PartyFecske <magonakos@gmail.com>
2025-05-08 18:31:49 +0200

    design valtozasok es kezdetleges navigacio

--- app/src/main/java/com/example/musicapp/MainActivity.java
+++ app/src/main/java/com/example/musicapp/MainActivity.java
@@ -3,10 +3,13 @@ package com.example.musicapp;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.ToggleButton;
+import androidx.appcompat.widget.Toolbar;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
@@ -14,97 +17,44 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import com.google.android.material.slider.Slider;
 
 public class MainActivity extends AppCompatActivity {
-    Slider slider;
-    Handler handler=new Handler();
-    Button playBtn, download;
-    TextView title;
-    ToggleButton repeat;
-    float progress=0;
-    boolean playing=false, isRepeating=false;
+    NavController navController;
+    NavHostFragment navHostFragment;
+    AppBarConfiguration appBarConfiguration;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+        //EdgeToEdge.enable(this);
 
-        setContentView(R.layout.fragment_music);
-        slider=findViewById(R.id.slider);
-        playBtn=findViewById(R.id.play);
-        title=findViewById(R.id.title);
-        repeat=findViewById(R.id.repeat);
-        download=findViewById(R.id.dowload);
+        setContentView(R.layout.activity_main);
+        toolbar=findViewById(R.id.mainToolbar);
+        setSupportActionBar(toolbar);
+        navHostFragment=(NavHostFragment)getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+        navController=navHostFragment.getNavController();
 
-        playBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                title.setText("Playing");
-                if(playing){
-                    handler.removeCallbacks(progressUpdater);
-                    playing=false;
-                    title.setText("Paused");
-                }
-                else {
-                    handler.postDelayed(progressUpdater, 1000);
-                    playing=true;
-                    title.setText("Playing");
-                }
-            }
-        });
-        repeat.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(v.isActivated()) {
-                    isRepeating = false;
-                    v.setActivated(false);
+        appBarConfiguration=new AppBarConfiguration.Builder(R.id.homeFragment,R.id.musicFragment).build();
 
-                }
-                else {
-                    isRepeating = true;
-                    v.setActivated(true);
-                }
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
 
-            }
-        });
-        slider.setValue(0);
-        slider.setValueTo(5);
-        slider.setStepSize(0.1f);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-        slider.addOnChangeListener(new Slider.OnChangeListener() {
-            @Override
-            public void onValueChange(@NonNull Slider slider, float value, boolean fromUser) {
-                if(fromUser){
-                    progress=value;
-                }
-            }
-        });
     }
-    private Runnable progressUpdater=new Runnable() {
-        @Override
-        public void run() {
-            progress+=1f;
-            if(progress<=100){
-                slider.setValue(progress);
-                handler.postDelayed(this,1000);
-            }
-            else if(isRepeating) {
-                progress = 0;
-                slider.setValue(progress);
-                handler.postDelayed(this,1000);
-            }
-            else {
-                handler.removeCallbacks(this);
-                title.setText("Playing");
-            }
-            Log.d("progress","progress: "+slider.getValue());
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        return NavigationUI.onNavDestinationSelected(item, navController)
+                || super.onOptionsItemSelected(item);
+    }
 
-        }
-    };
 }
