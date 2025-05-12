f8b6231ba2d853a7b25fa63a1a54a3c632ed756b
Markó Balázs Zsolt <marko.balazs0003@gmail.com>
2025-05-12 18:01:06 +0200

    song fragment changes

--- app/src/main/java/com/example/musicapp/ui/songlist/SongListFragment.java
+++ app/src/main/java/com/example/musicapp/ui/songlist/SongListFragment.java
@@ -1,4 +1,25 @@
 package com.example.musicapp.ui.songlist;
 
-public class SongListFragment {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.musicapp.MusicClient;
+import com.example.musicapp.R;
+
+public class SongListFragment extends Fragment {
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+
+        View view = inflater.inflate(R.layout.fragment_song_list, container, false);
+
+        RecyclerView songRecyclerView = view.findViewById(R.id.songRecyclerView);
+        //TODO FINISH IT
+        return view;
+    }
+
 }
